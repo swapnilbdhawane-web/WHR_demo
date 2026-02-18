0e74fd9766647c4c006ed0dc59f1be44ad078773
swapnilbdhawane-web <swapnil.b.dhawane@pwc.com>
2026-02-18 16:42:53 +0530

    Create main.tf

---
+++ infra/main.tf
@@ -0,0 +1,16 @@
+resource "aws_s3_bucket" "this" {
+  bucket = var.bucket_name
+}
+
+# Block all public access (recommended)
+resource "aws_s3_bucket_public_access_block" "this" {
+  bucket                  = aws_s3_bucket.this.id
+  block_public_acls       = true
+  block_public_policy     = true
+  ignore_public_acls      = true
+  restrict_public_buckets = true
+}
+
+output "bucket_name" {
+  value = aws_s3_bucket.this.bucket
+}
